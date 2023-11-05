55cdaf9aed1282c31880e0095ed0816b7e6c85d9
Subblack <osfel15102003@gmail.com>
2023-11-05 18:22:12 -0500

    Vistas

---
+++ ProyectoOL/Controllers/ClientController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace ProyectoOL.Controllers
+{
+    public class ClientController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DigitalLibrary()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoOL/Controllers/LibrarianController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoOL.Controllers
+{
+    public class LibrarianController : Controller
+    {
+        // GET: Librarian
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Books() 
+        {
+            return View();
+        }
+        public ActionResult People()
+        {
+            return View();
+        }
+    }
+}
